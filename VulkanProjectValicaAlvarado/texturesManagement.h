2e439fc96a9a286412ee5b0adf41217736c3952a
Pablo García Alvarado <e.pabloalvarado@go.ugr.es>
2024-06-14 17:56:54 +0200

    We have to do this new function

--- VulkanProjectValicaAlvarado/texturesManagement.h
+++ VulkanProjectValicaAlvarado/texturesManagement.h
@@ -472,6 +472,63 @@ void updateTextureImagesAdd(uint32_t &mipLevels, VkDevice &device, VkPhysicalDev
     textureImageMemorys.push_back(textureImageMemory);
 }
 
+/*
+void updateTextureImagesDelete(uint32_t &mipLevels, VkDevice &device, VkPhysicalDevice &physicalDevice,
+                            VkCommandPool &commandPool, VkQueue &graphicsQueue, std::vector<VkImage> &textureImages,
+                            std::vector<VkDeviceMemory> &textureImageMemorys, std::string newTexturePath){
+
+    int texWidth, texHeight, texChannels;
+    std::string texturePath = "textures/" + newTexturePath;
+    std::string texturePathWaterMelon = "textures/furniture/Laptop/SLT_Dif.png";
+
+    std::cout << texturePath + "\n";
+
+    stbi_uc* pixels = stbi_load(texturePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+    VkDeviceSize imageSize = texWidth * texHeight * 4;
+    mipLevels = static_cast<uint32_t>(std::floor(std::log2(std::max(texWidth, texHeight)))) + 1;
+
+    if (!pixels) {
+        throw std::runtime_error("failed to load texture image!");
+    }
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                 stagingBuffer, stagingBufferMemory, device, physicalDevice);
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+    memcpy(data, pixels, static_cast<size_t>(imageSize));
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    stbi_image_free(pixels);
+
+    VkImage textureImage;
+    VkDeviceMemory textureImageMemory;
+
+    createImage(texWidth, texHeight, mipLevels, VK_SAMPLE_COUNT_1_BIT, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL,
+                VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory, device, physicalDevice);
+
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB,
+                          VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, mipLevels,
+                          commandPool, device, graphicsQueue);
+    copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth),
+                      static_cast<uint32_t>(texHeight), commandPool, device, graphicsQueue);
+    //transitioned to VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL while generating mipmaps
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+
+    generateMipmaps(textureImage, VK_FORMAT_R8G8B8A8_SRGB, texWidth, texHeight, mipLevels, physicalDevice,
+                    device, commandPool, graphicsQueue);
+
+    textureImages.push_back(textureImage);
+    textureImageMemorys.push_back(textureImageMemory);
+}
+*/
+
 /*Inside this function two fixed paths are declare but they should be passed as parameters to be able to reuse the function*/
 void createTextureImage(uint32_t &mipLevels, VkDevice &device, VkPhysicalDevice &physicalDevice,
                         VkCommandPool &commandPool, VkQueue &graphicsQueue, VkImage &textureImage,
